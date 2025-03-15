6ce26f3b8338f6b9caf57b1101651d5eaa5d1a2b
Bdiaz18 <bdiaz.r18@gmail.com>
2025-03-15 10:10:04 +0000

    newcode

--- src/app.js
+++ src/app.js
@@ -1,68 +1,19 @@
 
 window.onload = function() {
-  //write your code here
   
+  function letmecreateanewcode() {
+    const parts = {
+      who: ['The dog', 'My grandma', 'The mailman', 'My bird', 'My cat', 'A stranger', 'The neighbor'],
+      action: ['ate', 'peed on', 'crushed', 'broke', 'stole', 'burned', 'hid'],
+      what: ['my homework', 'my phone', 'the car', 'my laptop', 'my shoes', 'the remote'],
+      when: ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying', 'right before my test', 'on my way home']
+    };
 
-    let who = [
-      "The dog",
-      "My grandma",
-      "The mailman",
-      "My bird",
-      "my girlfriend",
-      "my ex",
-      "My son",
-      "My cousin",
-      "A spider",
-      "My Stepsister",
-      "My Teacher"
-  
-    ];
-    let action = [
-      "ate",
-      "peed",
-      "crushed",
-      "broke",
-      "kicked",
-      "spat",
-      "burned",
-      "cursed",
-      "ignited",
-      "banished"
-    ];
-    let what = [
-      "my homework",
-      "my phone",
-      "the car",
-      "the sink",
-      "my pipe",
-      "my ps5",
-      "My PC",
-      "My Switch",
-      "My Hair"
-    
-  
-    ];
-    let when = [
-      "before the class",
-      "when I was sleeping",
-      "while I was exercising",
-      "during my lunch",
-      "while I was praying",
-      "when I was doing the JS project",
-      "during my 18 weeks with Antonio and Javier",
-      "In my last soccer match",
-      "When I was washing the dishes"
-  
-    ];
-  
-    let whos = who[Math.floor(Math.random() * who.length)];
-    let actions = action[Math.floor(Math.random() * action.length)];
-    let whats = what[Math.floor(Math.random() * what.length)];
-    let whens = when[Math.floor(Math.random() * when.length)];
-  
-    console.log(whos, actions, whats, whens);
-  
-    document.getElementById(
-      "excuse"
-    ).innerText = `${whos} ${actions} ${whats} ${whens}.`;
-  };
+    return Object.values(parts)
+      .map(arr => arr[Math.floor(Math.random() * arr.length)])
+      .join(" ") + ".";
+  }
+
+  document.getElementById("excuse").innerText = letmecreateanewcode();
+
+};
